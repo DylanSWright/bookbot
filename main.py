c0d935e597287fae31e5ecb8572d2e59bc292e78
DylDyl93 <d_wright93@icloud.com>
2024-12-23 08:15:07 -0600

    added main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+
+def sort_by_frequency(item):
+    return item[1]
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    
+    char_list = [(char, count) for char, count in chars_dict.items() if char.isalpha()]
+    char_list.sort(key=sort_by_frequency, reverse=True)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for char, count in char_list:
+        print(f"The '{char}' character was found {count} times")
+
+    print("--- End report ---")
+
+main()
